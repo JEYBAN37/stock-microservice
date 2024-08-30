96b5620298e7d850b27ed37058c1191c39b5aeb2
JEYBAN37 <jeyban37@gmail.com>
2024-08-29 23:57:39 -0500

    dto implement final hu6

--- src/main/java/com/example/stock/domain/article/model/dto/ArticleDto.java
+++ src/main/java/com/example/stock/domain/article/model/dto/ArticleDto.java
@@ -1,7 +1,7 @@
 package com.example.stock.domain.article.model.dto;
 
-import com.example.stock.domain.brand.model.entity.Brand;
-import com.example.stock.domain.category.model.entity.Category;
+import com.example.stock.domain.brand.model.dto.BrandDtoArticle;
+import com.example.stock.domain.category.model.dto.CategoryDtoArticle;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +18,7 @@ public class ArticleDto {
     private String description;
     private int quantity;
     private BigDecimal price;
-    private Brand brand;
-    private Category[] articleCategories;
+    private BrandDtoArticle brand;
+    private CategoryDtoArticle[] articleCategories;
+
 }
---
+++ src/main/java/com/example/stock/domain/brand/model/dto/BrandDtoArticle.java
@@ -0,0 +1,15 @@
+package com.example.stock.domain.brand.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BrandDtoArticle {
+    private Long id;
+    private String name;
+}
