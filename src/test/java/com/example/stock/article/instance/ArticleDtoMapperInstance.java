ca29f2d2fa94a654f4a57788ab9fc6fde54eb70f
JEYBAN37 <jeyban37@gmail.com>
2024-10-16 23:15:33 -0500

    refactor, fix and solutions changues

--- src/test/java/com/example/stock/article/instance/ArticleDtoMapperInstance.java
+++ src/test/java/com/example/stock/article/instance/ArticleDtoMapperInstance.java
@@ -1,8 +1,12 @@
 package com.example.stock.article.instance;
 
 import com.example.stock.application.articule.mapper.ArticleDtoMapper;
+import com.example.stock.domain.article.model.dto.ArticleCarDto;
 import com.example.stock.domain.article.model.dto.ArticleDto;
 import com.example.stock.domain.article.model.entity.Article;
+import com.example.stock.domain.category.model.dto.CategoryDtoArticle;
+
+import java.util.List;
 
 public class ArticleDtoMapperInstance implements ArticleDtoMapper {
     @Override
@@ -17,4 +21,23 @@ public class ArticleDtoMapperInstance implements ArticleDtoMapper {
 
         return dto;
     }
+
+    @Override
+    public ArticleCarDto cartoDto(Article objectOfDomain) {
+        if (objectOfDomain == null) {
+            return null;
+        }
+
+        ArticleCarDto dto = new ArticleCarDto();
+        dto.setId(objectOfDomain.getId());
+        dto.setQuantity(objectOfDomain.getQuantity());
+        List<CategoryDtoArticle> categoryDtoArticles = objectOfDomain
+                .getArticleCategories()
+                .stream()
+                .map( article -> new CategoryDtoArticle(article.getId(), article.getName())).toList();
+        dto.setArticleCategories(categoryDtoArticles);
+        return dto;
+    }
+
+
 }
